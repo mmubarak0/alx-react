5960b13b530d2c5bfad0b3c42a52b84c53a73b2d
Mohammed Mubarak <modymu9@gmail.com>
2024-07-23 03:53:37 +0200

    initialize project

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,8 @@
+import $ from "jquery";
+
+$(document).ready(function () {
+    $("body")
+        .append("<p>Holberton Dashboard</p>")
+        .append("<p>Dashboard data for the students</p>")
+        .append("<p>Copyright - Holberton School</p>");
+});
